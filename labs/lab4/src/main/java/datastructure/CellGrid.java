90acc0f722e0e902191f2ac7124914dd7d1268c8
Reinsdyret <lars.haukland@gmail.com>
2022-05-03 10:40:39 +0200

    Added all files

--- labs/lab4/src/main/java/datastructure/CellGrid.java
+++ labs/lab4/src/main/java/datastructure/CellGrid.java
@@ -6,12 +6,16 @@ public class CellGrid implements IGrid {
 	
 	int rows;
 	int columns;
+	CellState initialState;
 	
-	CellState[][] grid = new CellState[rows][columns];
+	CellState[][] grid;
     public CellGrid(int rows, int columns, CellState initialState) {
 		// TODO Auto-generated constructor stub
     	this.rows = rows;
     	this.columns = columns;
+    	this.initialState = initialState;
+    	
+    	grid = new CellState[rows][columns];
     	
     	for (int row = 0; row < grid.length; row++) {
 			for (int col = 0; col < grid[row].length; col++) {
@@ -47,7 +51,13 @@ public class CellGrid implements IGrid {
     @Override
     public IGrid copy() {
         // TODO Auto-generated method stub
-        return null;
+        IGrid copyCellGrid = new CellGrid(this.rows, this.columns, this.initialState);
+        for(int i = 0; i < this.grid.length; i++) {
+        	for(int j = 0; j < this.grid[i].length; j++) {
+        		copyCellGrid.set(i,j,this.grid[i][j]);
+        	}
+        }
+		return copyCellGrid;
     }
     
 }
